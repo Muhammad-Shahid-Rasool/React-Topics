ebf72bdf2da9ff4427f93ea200e72ecdc2060d8e
Muhammad-Shahid-Rasool <shahidrasool1529@gmail.com>
2023-02-01 22:30:13 +0500

    netlify-deployment-testing

--- src/Dua.jsx
+++ src/Dua.jsx
@@ -3,8 +3,9 @@
 function Dua() {
  
   return (
-   
-         <h3>Ya Allah Zahid ke Magfirat farmaye - Aameen</h3>
+   <div><h1>Allah o Akbar</h1>
+    <h3>Ya Allah Zahid ke Magfirat farmaye - Aameen</h3></div>
+        
  
   )
 }
